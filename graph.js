1aca8a04d64500b85e5a0529651407b2e35d9cf2
Bastos JT <bastosjt@gmail.com>
2025-03-07 16:37:41 +0100

    Initialisation du projet

--- graph.js
+++ graph.js
@@ -1,1 +1,15 @@
+// Variables pour la création du graphique
+const graphWidth = 600;
+const graphHeight = 200;
+const graphMargin = { top: 50, right: 30, bottom: 50, left: 60 };
 
+const svgGraph = d3.select("#graph")
+    .attr("width", graphWidth)
+    .attr("height", graphHeight)
+    .append("g")
+    .attr("transform", `translate(${graphMargin.left}, ${graphMargin.top})`);
+
+// Chargement des données CSV
+d3.csv("data.csv").then((data) => {
+    console.log("Données chargées :", data);
+});
