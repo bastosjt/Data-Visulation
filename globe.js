c2b2b0ba8fd83bb8e8ea720ec710cf4669e38c70
Carlos75clc <charleshenry75clc@gmail.com>
2025-03-20 16:02:05 +0100

    Features Terre

--- globe.js
+++ globe.js
@@ -1,4 +1,4 @@
-const width = 600, height = 600;
+const width = 1000, height = 600;
 const globeProjection = d3.geoOrthographic().scale(280).translate([width / 2, height / 2]);
 const globePath = d3.geoPath().projection(globeProjection);
 const svg = d3.select("#globe");
@@ -19,12 +19,42 @@ d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/w
         .attr("stroke", "#414861")
         .attr("d", globePath)
         .on("click", function(event, d) {
+            isRotating = false; 
             d3.selectAll(".country").transition().duration(300).attr("fill", "#242936");
             d3.select(this).transition().duration(300).attr("fill", "#ff7700");
+
+            const centroid = d3.geoCentroid(d);
+            d3.transition().duration(1000)
+                .tween("rotate", () => {
+                    const r = d3.interpolate(globeProjection.rotate(), [-centroid[0], -centroid[1]]);
+                    return t => {
+                        globeProjection.rotate(r(t));
+                        svg.selectAll("path").attr("d", globePath);
+                    };
+                })
+                .on("end", () => {
+                    d3.transition().duration(1000).tween("zoom", () => {
+                        const s = d3.interpolate(globeProjection.scale(), 400);
+                        return t => {
+                            globeProjection.scale(s(t));
+                            svg.selectAll("path").attr("d", globePath);
+                        };
+                    });
+
+                    setTimeout(() => {
+                        d3.transition().duration(1000).tween("zoomOut", () => {
+                            const s = d3.interpolate(globeProjection.scale(), 280);
+                            return t => {
+                                globeProjection.scale(s(t));
+                                svg.selectAll("path").attr("d", globePath);
+                            };
+                        });
+                        isRotating = true;
+                    }, 3000);
+                });
         })
         .on("mouseover", function(event, d) {
-            tooltip.style("visibility", "visible")
-                   .text(d.properties.name);
+            tooltip.style("visibility", "visible").text(d.properties.name);
         })
         .on("mousemove", function(event) {
             tooltip.style("top", (event.pageY - 10) + "px")
@@ -35,27 +65,19 @@ d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/w
         });
 });
 
-let lastX, lastY;
-let rotation = [0, 0];
-
-svg.call(d3.drag()
-    .on("start", (event) => {
-        lastX = event.x;
-        lastY = event.y;
-    })
-    .on("drag", (event) => {
-        const dx = event.x - lastX;
-        const dy = event.y - lastY;
-        rotation[0] += dx * 0.5;
-        rotation[1] -= dy * 0.5;
+let isRotating = true;
+function autoRotate() {
+    if (isRotating) {
+        const rotation = globeProjection.rotate();
+        rotation[0] += 0.15;
         globeProjection.rotate(rotation);
         svg.selectAll("path").attr("d", globePath);
-        lastX = event.x;
-        lastY = event.y;
-    })
-);
+    }
+    requestAnimationFrame(autoRotate);
+}
+autoRotate();
 
-// Ajout du tooltip pour afficher le nom du pays
+// Tooltip pour afficher le nom du pays
 const tooltip = d3.select("body").append("div")
     .style("position", "absolute")
     .style("background", "#1a1d27")
